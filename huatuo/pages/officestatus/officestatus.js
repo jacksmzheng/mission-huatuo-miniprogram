4dce3a3c5884eb839adf81d6ec9411c90f07091d
Jack S M Zheng <549632525@qq.com>
2020-02-05 11:08:12 +0800

    Update pulldown

--- huatuo/pages/officestatus/officestatus.js
+++ huatuo/pages/officestatus/officestatus.js
@@ -171,7 +171,8 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    this.refreshData();   
+    this.refreshData();
+    wx.stopPullDownRefresh();
   },
 
   /**
@@ -263,7 +264,7 @@ Page({
         if (res.statusCode == 200) {
           that.setData({
             newsList: res.data.returnObject.importantNewsResponseList,
-            ['btnNews.count']: res.data.returnObject.unReadCount
+            //['btnNews.count']: res.data.returnObject.unReadCount
           })
         } else {
           util.showErrorMessage(res.statusCode, res)

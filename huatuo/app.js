bcd7d5255feb8d4e9bdd21c04b265d146fee111b
Jack S M Zheng <549632525@qq.com>
2020-02-05 18:53:04 +0800

    update error message

--- huatuo/app.js
+++ huatuo/app.js
@@ -3,7 +3,7 @@ App({
   api: {
     isProdEnv: false,
     devUrl: 'https://huatuo-test.app77.cn',
-    prodUrl: 'https://huatuo.app77.cn'
+    prodUrl: 'https://huatuo.app77.cn/huatuo'
   },
   globalData: {
     appId: 'wx9812117be87d24d2',
